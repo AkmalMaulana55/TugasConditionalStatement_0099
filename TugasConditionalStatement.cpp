efe9fabdc91c8d5798b1cf2804284bd55c61c08c
AkmalMaulana55 <vylingsmall55@gmail.com>
2026-03-10 14:49:17 +0700

    membuat fungsi untuk menentukan status berat badan berdasarkan BMI

--- TugasConditionalStatement.cpp
+++ TugasConditionalStatement.cpp
@@ -14,3 +14,14 @@ void inputData(float &bb, float &tb){
 float hitungBMI(float bb, float tb){
     return bb / (tb * tb);
 }
+
+string statusBMI(float bmi){
+    if (bmi < 18.5)
+        return "Berat Badan Kurang";
+    else if (bmi < 25)
+        return "Berat Badan Normal";
+    else if (bmi < 30)
+        return "Berat Badan Kelebihan";
+    else
+        return "Obesitas";
+}
