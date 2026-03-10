a24a160eb9e4592ce9f3479b7a6437b47cb0ec53
AkmalMaulana55 <vylingsmall55@gmail.com>
2026-03-10 14:48:15 +0700

    membuat prosedur untuk memasukkan data berat dan tinggi

--- TugasConditionalStatement.cpp
+++ TugasConditionalStatement.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+void inputData(float &bb, float &tb){
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+
+    cout << "Masukkan berat badan (kg): ";
+    cin >> bb;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tb;
+}
